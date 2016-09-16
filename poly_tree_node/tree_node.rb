ddc16036f1f818e28eb269570d34a035a3c53acb
App Academy Student <student@appacademy.io>
2016-09-16 13:55:58 -0700

    finish PolyTreeNode#dfs and #bfs

--- poly_tree_node/tree_node.rb
+++ poly_tree_node/tree_node.rb
@@ -45,6 +45,26 @@ class PolyTreeNode
     end
   end
 
+  def dfs(value)
+    p self, value
+    return self if self.value == value
 
+    self.children.each do |child|
+      child_value = child.dfs(value)
+      return child_value if child_value
+    end
+    nil
+  end
+
+  def bfs(value)
+    queue = [self]
+
+    until queue.empty?
+      node = queue.shift
+      return node if node.value == value
+      queue += node.children
+    end
+    nil
+  end
   
 end
